ab4f41498b01523b9d2c4691399fd9d2c1791f49
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-02 14:31:43 +0200

    feat: added some example data to repo

--- backend/src/main/java/de/neuefische/backend/todo/TodoRepository.java
+++ backend/src/main/java/de/neuefische/backend/todo/TodoRepository.java
@@ -10,7 +10,17 @@ import java.util.Map;
 @Repository
 class TodoRepository {
 
-    private final Map<String, Todo> todos = new HashMap<>(Map.of("1", new Todo("1", "Test", TodoStatus.OPEN)));
+    private final Map<String, Todo> todos = new HashMap<>(
+            Map.of(
+                    "1", new Todo("1", "Test 1", TodoStatus.OPEN),
+                    "2", new Todo("2", "Test 2", TodoStatus.IN_PROGRESS),
+                    "3", new Todo("3", "Test 3", TodoStatus.DONE),
+                    "4", new Todo("4", "Test 4", TodoStatus.OPEN),
+                    "5", new Todo("5", "Test 5", TodoStatus.IN_PROGRESS),
+                    "6", new Todo("6", "Test 6", TodoStatus.OPEN),
+                    "7", new Todo("7", "Test 7", TodoStatus.OPEN)
+            )
+    );
 
     public List<Todo> getAll() {
         return new ArrayList<>(todos.values());
